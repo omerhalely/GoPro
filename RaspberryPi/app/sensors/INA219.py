dee0e890ff61216616cb409638157c21baa21504
Omer Halely <omerhalely6@gmail.com>
2025-10-21 23:22:40 +0300

    Fixed INA219 implementation

--- RaspberryPi/app/sensors/INA219.py
+++ RaspberryPi/app/sensors/INA219.py
@@ -1,9 +1,9 @@
-from adafruit_ina219 import INA219
+from adafruit_ina219 import INA219, ADCResolution, BusVoltageRange, Gain
 import board
 import busio
 
 
-class INA219:
+class ina219:
     def __init__(self, i2c):
         while not i2c.try_lock():
             pass
@@ -13,7 +13,16 @@ class INA219:
         self.ina219 = None
         if addresses:
             self.ina219 = INA219(i2c)
+        self.config()
     
+    def config(self):
+        if self.ina219:
+            self.ina219.bus_voltage_range = BusVoltageRange.RANGE_32V
+            self.ina219.gain = Gain.DIV_8_320MV
+            self.ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_128S
+            self.ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_128S
+            self.ina219.set_calibration_32V_2A()
+            
     def get_status(self):
         if self.ina219:
             return True
@@ -21,19 +30,19 @@ class INA219:
     
     def get_voltage(self):
         if self.ina219:
-            bus_voltage = self.ina.bus_voltage
-            shunt_voltage = self.ina.shunt_voltage
+            bus_voltage = self.ina219.bus_voltage
+            shunt_voltage = self.ina219.shunt_voltage
             return bus_voltage + shunt_voltage / 1000
         return 0
     
     def get_current(self):
         if self.ina219:
-            return self.ina.current
+            return self.ina219.current
         return 0
     
     def get_power(self):
         if self.ina219:
-            return self.ina.power
+            return self.ina219.power
         return 0
     
     def __repr__(self):
@@ -47,6 +56,21 @@ class INA219:
 
 if __name__ == "__main__":
     i2c = busio.I2C(board.SCL, board.SDA)
-    ina219 = INA219(i2c)
+    ina = ina219(i2c)
     
-    print(ina219)
+    calibration_length = 100
+    P = []
+    V = []
+    I = []
+    for i in range(calibration_length):
+        power = ina.get_power()
+        voltage = ina.get_voltage()
+        current = ina.get_current()
+        
+        P.append(power)
+        V.append(voltage)
+        I.append(current)
+    
+    print(f"Mean Power : {sum(P) / len(P)}")
+    print(f"Mean Voltage : {sum(V) / len(V)}")
+    print(f"Mean Current : {sum(I) / len(I)}")
